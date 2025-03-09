66e939a54687328e80f6caf845f2cf64299a33ac
major-sd <soujitdas30@gmail.com>
2025-03-09 12:10:46 +0530

    Added test cases

---
+++ tests/test_app.py
@@ -0,0 +1,20 @@
+import pytest
+import sys
+import os
+
+# Add project root directory to sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from app import app
+
+@pytest.fixture
+def client():
+    app.testing = True
+    return app.test_client()
+
+def test_home(client):
+    """Test if the home page returns 'Hello, DevOps!'"""
+    response = client.get('/')
+    print("hello")
+    assert response.status_code == 200
+    assert response.data.decode() == "Hello, DevOps!"
