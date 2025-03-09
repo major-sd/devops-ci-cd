6bfbaf3cbd8307e77a323e9b81d94f0a5537ab71
major-sd <soujitdas30@gmail.com>
2025-03-09 11:43:12 +0530

    Minor change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, DevOps World! Your CI/CD pipeline is working 🎉"
+    return "Hello, DevOps World! My CI/CD pipeline is working 🎉"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
